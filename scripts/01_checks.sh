3029acdb0764b239cb11a889fd8f377b2febd5a5
Seturaman Kumar <seturamankumar@gmail.com>
2026-06-13 20:19:04 +0530

    Added first two scripts

---
+++ scripts/01_checks.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# 01_checks.sh - Pre flight checks before installation
+
+# Loads the config.env for the variables and color functions
+source "$(dirname "$0")/config.env"
+
+# Checks internet connectivity
+check_internet() {
+
+  info "Checking Internet connectivity..."
+
+  if ! ping -c 2 -W 3 archlinux.org &>/dev/null; then
+    error "No internet connection detected. Please connect and retry."
+  fi
+
+  info "Internet connection OK."
+
+}
+
+# Checks type of boot (UEFI or BIOS)
+check_boot_mode() {
+
+  info "Detecting boot mode..."
+
+  if [ -d /sys/firmware/efi ]; then
+    BOOT_MODE="UEFI"
+    info "Boot mode: UEFI"
+  else
+    BOOT_MODE="BIOS"
+    warn "Boot mode: BIOS (Legacy). GRUB will be configured accordingly."
+  fi
+
+  echo "BOOT_MODE=${BOOT_MODE}" >>"$(dirname "$0")/config.env"
+
+}
+
+# Syncs system clocks
+sync_clock() {
+
+  info "Syncing system clock"
+  timedatectl set-ntp true
+  sleep 2
+  info "System clock synced."
+
+}
+
+# Run the checks
+check_internet
+check_boot_mode
+sync_clock
+
+info "All pre-flight checks passed."
---
+++ scripts/02_user_input.sh
@@ -0,0 +1,165 @@
+#!/bin/bash
+# 02_user_input.sh - Collect all user configuration before installations
+
+source "$(dirname "$0")/config.env"
+
+# Setting the hostname
+get_hostname() {
+
+  while true; do
+
+    info "Enter hostname for this machine:"
+    read -r HOSTNAME
+
+    if [[ -z "$HOSTNAME" ]]; then
+      warn "Hostname cannot be empty. Please try again."
+      continue
+    fi
+
+    if [[ ! "$HOSTNAME" =~ ^[a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?$ ]]; then
+      warn "Hostname can only contain letters, numbers, hyphens. Cannot start or end with a hyphen. Max 63 characters. Please try again."
+      continue
+    fi
+    break
+
+  done
+
+  echo "HOSTNAME=${HOSTNAME}" >>"$(dirname "$0")/config.env"
+  info "Hostname set to: ${HOSTNAME}"
+
+}
+
+# Setting the username
+get_username() {
+
+  while true; do
+
+    info "Enter username:"
+    read -r USERNAME
+
+    if [[ -z "$USERNAME" ]]; then
+      warn "Username cannot be empty. Please try again."
+      continue
+    fi
+
+    if [[ ! "$USERNAME" =~ ^[a-z_][a-z0-9_-]{0,31}$ ]]; then
+      warn "Username can only contain lowercase letters, numbers, hyphens, underscores. Must start with a letter or underscore. Max 32 characters. Please try again."
+      continue
+    fi
+
+  done
+
+  echo "Username=${USERNAME}" >>"$(dirname "$0")/config.env"
+  info "Username set to: ${USERNAME}"
+
+}
+
+# Setting the root password
+get_root_password() {
+
+  while true; do
+
+    info "Enter root password:"
+    read -rs ROOT_PASSWORD
+    echo
+
+    if [[ "$ROOT_PASSWORD" != "$ROOT_PASSWORD_CONFIRM" ]]; then
+      warn "Root passwords do not match. Please try again."
+      continue
+    fi
+
+    info "Confirm root password:"
+    read -rs ROOT_PASSWORD_CONFIRM
+    echo
+
+    if [[ -z "$ROOT_PASSWORD" ]]; then
+      warn "Root password cannot be empty. Please try again."
+      continue
+    fi
+
+  done
+
+  echo "ROOT_PASSWORD=${ROOT_PASSWORD}" >>"$(dirname "$0")/config.env"
+  info "Root password set."
+
+}
+
+# Setting the user password
+get_user_password() {
+
+  while true; do
+
+    info "Enter password for ${USERNAME}:"
+    read -rs USER_PASSWORD
+    echo
+
+    if [[ "$USER_PASSWORD" != "$USER_PASSWORD_CONFIRM" ]]; then
+      warn "User passwords do not match. Please try again."
+    fi
+
+    info "Confirm password for ${USERNAME}:"
+    read -rs USER_PASSWORD_CONFIRM
+    echo
+
+    if [[ -z "$USER_PASSWORD" ]]; then
+      warn "User password cannot be empty. Please try again."
+    fi
+
+  done
+
+  echo "USER_PASSWORD=${USER_PASSWORD}" >>"$(dirname "$0")/config.env"
+  info "User password set."
+
+}
+
+# Setting the timezone
+get_timezone() {
+
+  while true; do
+
+    info "Enter timezone (e.g. Asia/Kolkata, Europe/London), (default: Asia/Kolkata):"
+    info "Full list: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones"
+    read -r TIMEZONE
+    TIMEZONE="${TIMEZONE:-Asia/Kolkata}"
+
+    if ! timedatectl list-timezones | grep -qx "$TIMEZONE"; then
+      warn "Invalid timezone: ${TIMEZONE}. Please try again."
+      continue
+    fi
+
+  done
+
+  echo "TIMEZONE=${TIMEZONE}" >>"$(dirname "$0")/config.env"
+  info "Timezone set to: ${TIMEZONE}"
+
+}
+
+# Setting the locale
+get_locale() {
+
+  while true; do
+
+    info "Enter locale (e.g. en_US.UTF-8, en_IN.UTF-8), (default: en_US.UTF-8):"
+    read -r LOCALE
+    LOCALE="${LOCALE:-en_US.UTF-8}"
+
+    if ! grep -q "^#\?$(echo $LOCALE | sed 's/\./\\./g')" /etc/locale.gen; then
+      warn "Invlaid locale: ${LOCALE}. Please try again."
+      continue
+    fi
+
+  done
+
+  echo "LOCALE=${LOCALE}" >>"$(dirname "$0")"/config.env
+  info "Locale set to: ${LOCALE}"
+
+}
+
+get_hostname
+get_username
+get_root_password
+get_user_password
+get_timezone
+get_locale
+
+info "All user configuration collected."
