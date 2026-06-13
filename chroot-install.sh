7ebb93df58bb0e5d557abf71db3f71cfadd12d01
Seturaman Kumar <seturamankumar@gmail.com>
2026-06-13 21:28:10 +0530

    Updated the chroot-install.sh for new system configuration

---
+++ chroot-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# chroot-install.sh - Runs inside arch-chroot, configures the new system with packages
+
+source /auto-arch-install/scripts/config.env
+
+bash /auto-arch-install/scripts/05_chroot.sh
+bash /auto-arch-install/scripts/06_aur.sh
